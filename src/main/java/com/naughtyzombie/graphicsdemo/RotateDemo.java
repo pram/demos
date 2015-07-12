ed9916a0cd9e3328fd7eee21638c768f151316f0
pram <pram@attale.com>
2015-07-12 14:39:46 +0100

    formatting

--- src/main/java/com/naughtyzombie/graphicsdemo/RotateDemo.java
+++ src/main/java/com/naughtyzombie/graphicsdemo/RotateDemo.java
@@ -26,20 +26,18 @@ public class RotateDemo {
                 try {
                     image = ImageIO.read(RotateDemo.class.getResource("/london.jpg"));
 
-
                     // The required drawing location
                     int drawLocationX = 300;
                     int drawLocationY = 300;
 
-// Rotation information
-
+                    // Rotation information
                     double rotationRequired = Math.toRadians(45);
                     double locationX = image.getWidth() / 2;
                     double locationY = image.getHeight() / 2;
                     AffineTransform tx = AffineTransform.getRotateInstance(rotationRequired, locationX, locationY);
                     AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
 
-// Drawing the rotated image at the required drawing locations
+                    // Drawing the rotated image at the required drawing locations
                     //g2d.drawImage(op.filter(image, null), drawLocationX, drawLocationY, null);
 
                 } catch (Exception e) {
