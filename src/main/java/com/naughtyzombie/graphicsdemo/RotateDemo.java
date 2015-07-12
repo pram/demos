014ec28723899b92c8443f5a6d5fffa1173ea219
pram <pram@attale.com>
2015-07-12 15:49:41 +0100

    working rotation

--- src/main/java/com/naughtyzombie/graphicsdemo/RotateDemo.java
+++ src/main/java/com/naughtyzombie/graphicsdemo/RotateDemo.java
@@ -25,6 +25,7 @@ public class RotateDemo {
                 BufferedImage image = null;
                 try {
                     image = ImageIO.read(RotateDemo.class.getResource("/london.jpg"));
+                    int type = image.getType();
 
                     int[][] imgMatrix = new int[image.getHeight()][image.getWidth()];
 
@@ -33,10 +34,23 @@ public class RotateDemo {
 
                     for (int i=0; i < rows; i++) {
                         for (int j=0; j < columns; j++) {
-                            //System.out.println(i + " - " + j);
                             imgMatrix[i][j] = image.getRGB(j,i);
                         }
                     }
+
+                    imgMatrix = rotateCW(imgMatrix);
+
+                    rows = imgMatrix.length;
+                    columns = imgMatrix[0].length;
+                    image = new BufferedImage(columns,rows,type);
+
+                    for (int i=0; i < rows; i++) {
+                        for (int j=0; j < columns; j++) {
+                            image.setRGB(j,i, imgMatrix[i][j]);
+                        }
+                    }
+
+
 
                     // The required drawing location
                     /*int drawLocationX = 300;
@@ -66,5 +80,66 @@ public class RotateDemo {
                 editorFrame.setVisible(true);
             }
         });
+    }
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        if (n <= 1) {
+            return; // nothing to do
+        }
+
+        /* layers */
+        for (int i = 0; i < n / 2; i++) {
+            /* elements */
+            for (int j = i; j < n - i - 1; j++) {
+                int saved = matrix[i][j];
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = saved;
+            }
+        }
+    }
+
+    public static void rotate2(int[][] matrix) {
+        transpose(matrix);
+        reflect(matrix);
+    }
+
+    public static void reflect(int[][] arr) {
+        int size = arr.length;
+        for (int k = 0; k < size; k++) {
+            for (int i = 0; i < Math.floor(size / 2); i++) {
+                swap(arr, k, i, k, size - i - 1);
+            }
+        }
+    }
+
+    public static void transpose(int[][] arr) {
+        int size = arr.length;
+        for (int diag = 0; diag < size; diag++) {
+            for (int i = diag + 1; i < size; i++) {
+                swap(arr, diag, i, i, diag);
+            }
+        }
+    }
+
+    public static void swap(int[][] arr, int row1, int col1, int row2, int col2) {
+        int num1 = arr[row1][col1];
+        int num2 = arr[row2][col2];
+        arr[row1][col1] = num2;
+        arr[row2][col2] = num1;
+    }
+
+    public int[][] rotateCW(int[][] mat) {
+        final int M = mat.length;
+        final int N = mat[0].length;
+        int[][] ret = new int[N][M];
+        for (int r = 0; r < M; r++) {
+            for (int c = 0; c < N; c++) {
+                ret[c][M-1-r] = mat[r][c];
+            }
+        }
+        return ret;
     }
 }
