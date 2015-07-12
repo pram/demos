41e9309a47062204b5eaea4c03eb867922af0ced
pram <pram@attale.com>
2015-07-12 15:52:53 +0100

    tidy up

--- src/main/java/com/naughtyzombie/graphicsdemo/RotateDemo.java
+++ src/main/java/com/naughtyzombie/graphicsdemo/RotateDemo.java
@@ -1,9 +1,7 @@
 package com.naughtyzombie.graphicsdemo;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.image.*;
-import java.io.*;
 import javax.imageio.*;
 import javax.swing.*;
 
@@ -38,7 +36,7 @@ public class RotateDemo {
                         }
                     }
 
-                    imgMatrix = rotateCW(imgMatrix);
+                    imgMatrix = rotate(imgMatrix);
 
                     rows = imgMatrix.length;
                     columns = imgMatrix[0].length;
@@ -49,23 +47,6 @@ public class RotateDemo {
                             image.setRGB(j,i, imgMatrix[i][j]);
                         }
                     }
-
-
-
-                    // The required drawing location
-                    /*int drawLocationX = 300;
-                    int drawLocationY = 300;*/
-
-                    // Rotation information
-                    /*double rotationRequired = Math.toRadians(45);
-                    double locationX = image.getWidth() / 2;
-                    double locationY = image.getHeight() / 2;
-                    AffineTransform tx = AffineTransform.getRotateInstance(rotationRequired, locationX, locationY);
-                    AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);*/
-
-                    // Drawing the rotated image at the required drawing locations
-                    //g2d.drawImage(op.filter(image, null), drawLocationX, drawLocationY, null);
-
                 } catch (Exception e) {
                     e.printStackTrace();
                     System.exit(1);
@@ -82,56 +63,7 @@ public class RotateDemo {
         });
     }
 
-    public static void rotate(int[][] matrix) {
-        int n = matrix.length;
-        if (n <= 1) {
-            return; // nothing to do
-        }
-
-        /* layers */
-        for (int i = 0; i < n / 2; i++) {
-            /* elements */
-            for (int j = i; j < n - i - 1; j++) {
-                int saved = matrix[i][j];
-                matrix[i][j] = matrix[n - j - 1][i];
-                matrix[n - j - 1][i] = matrix[n - 1 - i][n - 1 - j];
-                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
-                matrix[j][n - 1 - i] = saved;
-            }
-        }
-    }
-
-    public static void rotate2(int[][] matrix) {
-        transpose(matrix);
-        reflect(matrix);
-    }
-
-    public static void reflect(int[][] arr) {
-        int size = arr.length;
-        for (int k = 0; k < size; k++) {
-            for (int i = 0; i < Math.floor(size / 2); i++) {
-                swap(arr, k, i, k, size - i - 1);
-            }
-        }
-    }
-
-    public static void transpose(int[][] arr) {
-        int size = arr.length;
-        for (int diag = 0; diag < size; diag++) {
-            for (int i = diag + 1; i < size; i++) {
-                swap(arr, diag, i, i, diag);
-            }
-        }
-    }
-
-    public static void swap(int[][] arr, int row1, int col1, int row2, int col2) {
-        int num1 = arr[row1][col1];
-        int num2 = arr[row2][col2];
-        arr[row1][col1] = num2;
-        arr[row2][col2] = num1;
-    }
-
-    public int[][] rotateCW(int[][] mat) {
+    public int[][] rotate(int[][] mat) {
         final int M = mat.length;
         final int N = mat[0].length;
         int[][] ret = new int[N][M];
