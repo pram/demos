27bcf3ff46d154ebe398ded0556334a3c975ccb9
pram <pram@attale.com>
2015-07-12 15:28:32 +0100

    loading into an array

---
+++ src/main/java/com/naughtyzombie/graphicsdemo/ImageTest.java
@@ -0,0 +1,102 @@
+package com.naughtyzombie.graphicsdemo;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.io.IOException;
+import java.util.Random;
+
+public class ImageTest {
+
+    private static final int HEIGHT = 250;
+    private static final int WIDTH = 250;
+
+    public static void main(String[] args) throws IOException {
+        /*double[][] data = new double[WIDTH][HEIGHT];
+        Random r = new Random();
+        for(int i = 0; i < WIDTH; i++) {
+            for(int j = 0; j < HEIGHT; j++) {
+                data[i][j] = r.nextDouble();
+            }
+        }*/
+
+        BufferedImage hugeImage = ImageIO.read(PerformanceTest.class.getResource("/london.jpg"));
+        double[][] data = convertTo2DWithoutUsingGetRGB(hugeImage);
+
+        final BufferedImage img = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g = (Graphics2D)img.getGraphics();
+        for(int i = 0; i < WIDTH; i++) {
+            for(int j = 0; j < HEIGHT; j++) {
+                float c = (float) data[i][j];
+                g.setColor(new Color(c, c, c));
+                g.fillRect(i, j, 1, 1);
+                //data[i][j] = r.nextDouble();
+            }
+        }
+
+        JFrame frame = new JFrame("Image test");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                Graphics2D g2d = (Graphics2D)g;
+                g2d.clearRect(0, 0, getWidth(), getHeight());
+                g2d.setRenderingHint(
+                        RenderingHints.KEY_INTERPOLATION,
+                        RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+                        // Or _BICUBIC
+                g2d.scale(2, 2);
+                g2d.drawImage(img, 0, 0, this);
+            }
+        };
+        panel.setPreferredSize(new Dimension(WIDTH*2, HEIGHT*2));
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static double[][] convertTo2DWithoutUsingGetRGB(BufferedImage image) {
+
+        final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+        final int width = image.getWidth();
+        final int height = image.getHeight();
+        final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+
+        double[][] result = new double[height][width];
+        if (hasAlphaChannel) {
+            final int pixelLength = 4;
+            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+                int argb = 0;
+                argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
+                argb += ((int) pixels[pixel + 1] & 0xff); // blue
+                argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
+                argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
+                result[row][col] = argb;
+                col++;
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        } else {
+            final int pixelLength = 3;
+            for (int pixel = 0, row = 0, col = 0; pixel < pixels.length; pixel += pixelLength) {
+                int argb = 0;
+                argb += -16777216; // 255 alpha
+                argb += ((int) pixels[pixel] & 0xff); // blue
+                argb += (((int) pixels[pixel + 1] & 0xff) << 8); // green
+                argb += (((int) pixels[pixel + 2] & 0xff) << 16); // red
+                result[row][col] = argb;
+                col++;
+                if (col == width) {
+                    col = 0;
+                    row++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
--- src/main/java/com/naughtyzombie/graphicsdemo/RotateDemo.java
+++ src/main/java/com/naughtyzombie/graphicsdemo/RotateDemo.java
@@ -26,16 +26,28 @@ public class RotateDemo {
                 try {
                     image = ImageIO.read(RotateDemo.class.getResource("/london.jpg"));
 
+                    int[][] imgMatrix = new int[image.getHeight()][image.getWidth()];
+
+                    int rows = imgMatrix.length;
+                    int columns = imgMatrix[0].length;
+
+                    for (int i=0; i < rows; i++) {
+                        for (int j=0; j < columns; j++) {
+                            //System.out.println(i + " - " + j);
+                            imgMatrix[i][j] = image.getRGB(j,i);
+                        }
+                    }
+
                     // The required drawing location
-                    int drawLocationX = 300;
-                    int drawLocationY = 300;
+                    /*int drawLocationX = 300;
+                    int drawLocationY = 300;*/
 
                     // Rotation information
-                    double rotationRequired = Math.toRadians(45);
+                    /*double rotationRequired = Math.toRadians(45);
                     double locationX = image.getWidth() / 2;
                     double locationY = image.getHeight() / 2;
                     AffineTransform tx = AffineTransform.getRotateInstance(rotationRequired, locationX, locationY);
-                    AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);
+                    AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_BILINEAR);*/
 
                     // Drawing the rotated image at the required drawing locations
                     //g2d.drawImage(op.filter(image, null), drawLocationX, drawLocationY, null);
